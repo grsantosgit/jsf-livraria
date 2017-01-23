460a089b60fb7531feef11cf212f3a44f128e706
Guilherme Rosa <guilherme.rosa.santos@outlook.com>
2017-01-23 20:16:12 -0200

    Subindo projeto Livraria com JSF v1

---
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -0,0 +1,77 @@
+package br.com.caelum.livraria.bean;
+
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+
+import br.com.caelum.livraria.dao.DAO;
+import br.com.caelum.livraria.modelo.Autor;
+import br.com.caelum.livraria.modelo.Livro;
+
+@ManagedBean
+@ViewScoped
+public class LivroBean {
+
+	private Livro livro = new Livro();
+	private Integer autorId;
+	
+	public Integer getAutorId() {
+		return autorId;
+	}
+	
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
+	}
+
+	public Livro getLivro() {
+		return livro;
+	}
+	
+	public List<Autor> getAutores(){
+		return new DAO<Autor>(Autor.class).listaTodos();
+	}
+	
+	public void gravarAutor(){
+		Autor autor = new DAO<Autor>(Autor.class).buscaPorId(this.autorId);
+		this.livro.adicionaAutor(autor);
+	}
+	
+	public List<Autor> getAutoresDoLivro(){
+		return this.livro.getAutores();
+	}
+	
+	public List<Livro> getLivros() {
+		  return new DAO<Livro>(Livro.class).listaTodos();
+	}
+
+	public void gravar() {
+		System.out.println("Gravando livro " + this.livro.getTitulo());
+
+		if (livro.getAutores().isEmpty()) {
+			//throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+			FacesContext.getCurrentInstance().addMessage("autor", new FacesMessage("Livro deve ter pelo menos um Autor"));
+			return;
+		}
+
+		new DAO<Livro>(Livro.class).adiciona(this.livro);
+	}
+	
+	public String formAutor(){
+		System.out.println("Chamndo o formulário de autores");
+		return "autor?faces-redirect=true";
+	}
+	
+	public void comecaComDigitoUm(FacesContext fc, UIComponent component, Object value)throws ValidatorException{
+		String valor = value.toString();
+		if(!valor.startsWith("1")){
+			throw new ValidatorException(new FacesMessage("Deveria começar com um"));
+		}
+		
+	}
+
+}
